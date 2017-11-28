20c05edaf01d266fb9ff00df4cfdf6868f2b3e63
Calvin Karundu <calvin.mwenda@gmail.com>
2017-11-28 14:57:41 +0300

    Finalize slack upload and post message logic

--- src/modules/slack/index.js
+++ src/modules/slack/index.js
@@ -1,12 +1,74 @@
+import fs from 'fs';
 import config from 'config';
-import { WebClient } from '@slack/client';
+import request from 'request';
 
 const slackConfig = config.get('slack');
 
-export const uploadFile = options => new Promise((resolve, reject) => {
-  // Upload code here...
+export const postChatMessage = message => new Promise((resolve, reject) => {
+  const {
+    responseUrl = undefined,
+    channel = null,
+    text = null,
+    attachments = null,
+    replaceOriginal = null,
+  } = message;
+
+  const payload = {
+    response_type: 'in_channel',
+  };
+
+  if (channel !== null) payload.channel = channel;
+  if (text !== null) payload.text = text;
+  if (attachments !== null) payload.attachments = attachments;
+  if (replaceOriginal !== null) payload.replace_original = replaceOriginal;
+
+  request.post({
+    url: responseUrl,
+    body: payload,
+    json: true,
+  }, (err, response, body) => {
+    if (err) {
+      reject(err);
+    } else if (response.statusCode !== 200) {
+      reject(body);
+    } else if (body.ok !== true) {
+      reject(new Error(`Got non ok response while posting chat message body ${JSON.stringify(body, 0, 4)}`));
+    } else {
+      resolve(body);
+    }
+  });
 });
 
-export const postChatMessage = message => new Promise((resolve, reject) => {
-  // Post chat code here...
+export const uploadFile = options => new Promise((resolve, reject) => {
+  const {
+    filePath,
+    fileTmpName,
+    fileName,
+    fileType,
+  } = options;
+
+  const payload = {
+    token: slackConfig.reporterBot.botToken,
+    file: fs.createReadStream(filePath),
+    channels: slackConfig.reporterBot.fileUploadChannel,
+    filetype: fileType,
+    filename: fileTmpName,
+    title: fileName,
+  };
+
+  request.post({
+    url: slackConfig.fileUploadUrl,
+    formData: payload,
+    json: true,
+  }, (err, response, body) => {
+    if (err) {
+      reject(err);
+    } else if (response.statusCode !== 200) {
+      reject(body);
+    } else if (body.ok !== true) {
+      reject(new Error(`Got non ok response while uploading file ${fileTmpName} body ${JSON.stringify(body, 0, 4)}`));
+    } else {
+      resolve(body);
+    }
+  });
 });
